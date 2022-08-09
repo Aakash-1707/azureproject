0f5119f0e31f41cb81f2b3dd7ce3e59de2302a27
Joel981 <75721103+Joel981@users.noreply.github.com>
2022-08-09 11:00:28 +0530

    Add project files.

---
+++ devopsl2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devopsl2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
